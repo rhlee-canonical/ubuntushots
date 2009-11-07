20f69330ce6f4909e3450e6992aee4a7c94bd2d3
Christoph Haas <email@christoph-haas.de>
2009-11-07 17:05:44 +0100

    Version 0.4.6.1
    Signed-off-by: Christoph Haas <email@christoph-haas.de>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='debshots',
-    version="0.4.6",
+    version="0.4.6.1",
     description='Web application that manages screenshots of applications available in Debian',
     author='Christoph Haas',
     author_email='email@christoph-haas.de',
