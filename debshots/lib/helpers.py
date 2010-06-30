64f5db36e8f4945aac317fb1416023f46fc31224
Christoph Haas <email@christoph-haas.de>
2010-06-30 23:46:58 +0200

    Add helpers to provide links to upstream packages and bugs pages

--- debshots/lib/helpers.py
+++ debshots/lib/helpers.py
@@ -12,3 +12,23 @@ from webhelpers import paginate
 from pylons import url
 from routes.util import url_for
 from debshots.lib import my
+
+def package_page_link(package):
+    """Return the URL to the package page depending on the origin.
+
+    For Ubuntu packages it points to packages.ubuntu.com while for all other packages it
+    points to packages.debian.org."""
+    if package.origin.lower()=='ubuntu':
+        return tags.link_to("Package page on ubuntu.com", "http://packages.ubuntu.com/%s" % package.name)
+    else:
+        return tags.link_to("Package page on debian.org", "http://packages.debian.org/%s" % package.name)
+
+def bugs_page_link(package):
+    """Return the URL to the bug reports page depending on the origin.
+
+    For Ubuntu packages it points to edge.launchpad.net while for all other packages it
+    points to bugs.debian.org."""
+    if package.origin.lower()=='ubuntu':
+        return tags.link_to("Bug reports", "https://launchpad.net/distros/ubuntu/+source/%s/+bugs" % package.name)
+    else:
+        return tags.link_to("Bug reports", "http://bugs.debian.org/%s" % package.name)
