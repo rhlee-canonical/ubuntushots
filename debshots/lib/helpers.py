778c3190b147cb79aa145772611c525531884c01
Christoph Haas <email@christoph-haas.de>
2010-07-24 11:56:48 +0200

    Fix: Cope with packages that have no origin

--- debshots/lib/helpers.py
+++ debshots/lib/helpers.py
@@ -18,7 +18,7 @@ def package_page_link(package):
 
     For Ubuntu packages it points to packages.ubuntu.com while for all other packages it
     points to packages.debian.org."""
-    if package.origin.lower()=='ubuntu':
+    if package.origin is not None and package.origin.lower()=='ubuntu':
         return tags.link_to("Package page on ubuntu.com", "http://packages.ubuntu.com/%s" % package.name)
     else:
         return tags.link_to("Package page on debian.org", "http://packages.debian.org/%s" % package.name)
@@ -28,7 +28,7 @@ def bugs_page_link(package):
 
     For Ubuntu packages it points to edge.launchpad.net while for all other packages it
     points to bugs.debian.org."""
-    if package.origin.lower()=='ubuntu':
+    if package.origin is not None and package.origin.lower()=='ubuntu':
         return tags.link_to("Bug reports", "https://launchpad.net/distros/ubuntu/+source/%s/+bugs" % package.name)
     else:
         return tags.link_to("Bug reports", "http://bugs.debian.org/%s" % package.name)
